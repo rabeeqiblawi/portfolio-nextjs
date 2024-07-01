d5e6fe97eeb5e042e5d71fef315e5ed9f6ece3b7
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2024-07-01 11:27:14 +0300

    loading Page with style

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -5,7 +5,9 @@ import { config as websiteInformation } from '@/config'
 const Loading = () => {
     return (
         <div className={styles.loadingContainer}>
-            <Image src={websiteInformation.content.logo} alt="" width={35} height={35} className={styles.imgLogo} />
+            <div className={styles.loadingBox}>
+                <Image src={websiteInformation.content.logo} alt="" width={35} height={35} className={styles.imgLogo} />
+            </div>
             <h3>Loading...</h3>
         </div>
     )
