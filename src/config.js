6f05b403173fa517abbf76a5beb1d1b8f6b07a93
rabeekiblawi <rabeekiblawi@gmail.com>
2024-07-06 23:47:49 +0300

    added bookifai project

--- src/config.js
+++ src/config.js
@@ -19,9 +19,9 @@ export const config = {
             isHidden: false, 
             data:[
                 {
-                    imageUrl: "/websiteProjectPlaceholder1.png",
-                    title: "Bluehost Design Website",
-                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+                    imageUrl: "/bookifai.jpeg",
+                    title: "Bookifai",
+                    description: "Like reading novels? Ever wanted to connect with the characters in the book and chat with them? Bookifai allows you to just do that! You upload your favorite book and we extract the characters and create a chatbots for you to interact with them using discord!",
                     actionLink: "",
                     actionText: "View Project",
                     isHidden: false,
