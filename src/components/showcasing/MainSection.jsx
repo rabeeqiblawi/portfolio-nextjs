bdac4864e4ca342423693a5e0dd5d99b2e4de658
rabeekiblawi <rabeekiblawi@gmail.com>
2024-07-08 11:54:36 +0300

    improvements in parallax effect

--- src/components/showcasing/MainSection.jsx
+++ src/components/showcasing/MainSection.jsx
@@ -1,8 +1,6 @@
-
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './MainSection.module.scss'
 import Link from 'next/link';
-import Image from 'next/image';
 import { config } from '@/config';
 import ParallaxGallery from '../parallax/ParallaxGallery';
 
@@ -14,11 +12,32 @@ const MainSection = () => {
         'parallax-effect/2.png',
         'parallax-effect/3.png',
         'parallax-effect/4.png',
-    ]
-    console.log(images);
+    ];
+
+    const [hoverPosition, setHoverPosition] = useState({ x: 0, y: 0 });
+    const containerRef = useRef(null);
+
+    const handleMouseMove = (e) => {
+        if (containerRef.current) {
+            const { left, top, width, height } = containerRef.current.getBoundingClientRect();
+            const x = (e.clientX - left) / width;
+            const y = (e.clientY - top) / height;
+            setHoverPosition({ x, y });
+        }
+    };
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (container) {
+            container.addEventListener('mousemove', handleMouseMove);
+            return () => {
+                container.removeEventListener('mousemove', handleMouseMove);
+            };
+        }
+    }, []);
 
     return (
-        <div className={styles.container}>
+        <div className={styles.container} ref={containerRef}>
             <div className={styles.content}>
                 <div className={styles.textContainer}>
                     <h1>
@@ -28,19 +47,15 @@ const MainSection = () => {
                                     <span>{word}</span>
                                     : ` ${word} `
                                 }
-                                {breakLineAfter.includes(word) && <br />} {/* Add <br /> after specified words */}
+                                {breakLineAfter.includes(word) && <br />}
                             </React.Fragment>
-                            
                         ))}
                     </h1>
                     <div className={styles.line}></div>
                     <Link href="/services">Services</Link>
                 </div>
-                {/* <div className={styles.imgContainer}>
-                    <Image src="/heroImage.png" width={450} height={550} />
-                </div> */}
                 <div className={styles.imgContainer}>
-                    <ParallaxGallery images={images} />
+                    <ParallaxGallery images={images} hoverPosition={hoverPosition} />
                 </div>
             </div>
         </div>
