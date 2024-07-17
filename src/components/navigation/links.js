741e0419212f615709cb7fbe99e00a8a7258fc3d
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2024-07-17 11:42:44 +0300

    Update links.js
    add template

--- src/components/navigation/links.js
+++ src/components/navigation/links.js
@@ -2,6 +2,7 @@ const links = [
   { path: '/#projects', title: 'Projects' },
   { path: '/blog', title: 'Blog' },
   { path: '/#solutions', title: 'Solutions' },
+  // { path: '/templates', title: 'Templates' },
   // { path: '/services', title: 'Services' },
   { path: '/#contact', title: 'Contact' },
 ];
