da0b586fe74db5a5281e2ea0e9e7b06e2bf55e9e
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2024-07-20 19:29:55 +0300

    commin soon update (not done)

--- src/components/cards/InfoCard.jsx
+++ src/components/cards/InfoCard.jsx
@@ -3,16 +3,23 @@ import styles from './InfoCard.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const InfoCard = ({ icon, title, description, actionText, actionLink }) => {
+const InfoCard = ({ icon, title, description, actionText, actionLink, commingSoon }) => {
+    commingSoon ="/icons/soon.svg";
     return (
         <div className={styles.container}>
             <div className={styles.content}>
                 <Image src={icon} alt={title} className={styles.icon} width={35} height={35} />
                 <h2 className={styles.title}>{title}</h2>
                 <p className={styles.description}>{description}</p>
-                <Link href={actionLink} className={styles.buttonDiv}>
-                    <button className={styles.previewWebsiteButton}>{actionText}</button>
-                </Link>
+                {actionText &&
+                    <Link href={actionLink} target="_blank" className={styles.buttonDiv}>
+                        <button className={styles.previewWebsiteButton}>{actionText}</button>
+                    </Link>
+                }
+                {commingSoon && 
+                    <Image src={commingSoon} alt={title} className={styles.comingSoon} width={70} height={70} />
+                }
+
             </div>
         </div>
     );
