3f3a8be7d89a7059e343d92864568eb69f51b92f
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2024-07-20 11:28:48 +0300

    fix action link on solutionSection

--- src/components/cards/InfoCard.jsx
+++ src/components/cards/InfoCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './InfoCard.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const InfoCard = ({ icon, title, description, actionText }) => {
+const InfoCard = ({ icon, title, description, actionText, actionLink }) => {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
                 <Image src={icon} alt={title} className={styles.icon} width={35} height={35} />
                 <h2 className={styles.title}>{title}</h2>
                 <p className={styles.description}>{description}</p>
-                <div className={styles.buttonDiv}>
+                <Link href={actionLink} className={styles.buttonDiv}>
                     <button className={styles.previewWebsiteButton}>{actionText}</button>
-                </div>
+                </Link>
             </div>
         </div>
     );
