fb2f4591c908b4582593a4c2627b9a67435a12e1
rabeekiblawi <rabeekiblawi@gmail.com>
2024-07-22 21:07:15 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,9 +8,15 @@ import { config as websiteInformation } from '@/config'
 
 const inter = Inter({ subsets: ["latin"] });
 
+// export const metadata = {
+//   title: websiteInformation.content.brand,
+//   description: websiteInformation.content.metaDescription,
+//   icon: websiteInformation.content.logo,
+// };
+
 export const metadata = {
-  title: websiteInformation.content.brand,
-  description: websiteInformation.content.metaDescription,
+  title: "test test",
+  description: "test 2 test 2",
   icon: websiteInformation.content.logo,
 };
 
