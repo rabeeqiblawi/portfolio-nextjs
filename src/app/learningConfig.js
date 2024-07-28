418d123f35a84b49fe7bef19c946ba44e0df51d3
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2024-07-28 16:57:27 +0300

    some update with learning section

--- src/app/learningConfig.js
+++ src/app/learningConfig.js
@@ -1,7 +1,7 @@
 export const learningConfig = {
     courses:{
         isHidden: false,
-        mainTitle: 'Your Courses',
+        mainTitle: 'Available Courses',
         mainTitleHighlightedWord: 'Courses',
         data: [
             {
